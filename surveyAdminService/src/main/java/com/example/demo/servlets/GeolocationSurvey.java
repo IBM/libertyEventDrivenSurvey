a74f032778becf0470ab548c99a3d86ea8fdbe78
Kevin Grigorenko <kevin.grigorenko@us.ibm.com>
2023-06-28 13:02:14 -0500

    Add survey administration home page
    Signed-off-by: Kevin Grigorenko <kevin.grigorenko@us.ibm.com>

---
+++ surveyAdminService/src/main/java/com/example/demo/servlets/GeolocationSurvey.java
@@ -0,0 +1,19 @@
+package com.example.demo.servlets;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("GeolocationSurvey")
+public class GeolocationSurvey extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/plain");
+		response.getWriter().println("Hello World");
+	}
+}
