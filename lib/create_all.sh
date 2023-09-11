333ccf04b2e976f75e5043b154dbce640a1ca59f
Kevin Grigorenko <kevin.grigorenko@us.ibm.com>
2023-09-11 12:41:07 -0700

    Add convenience script
    Signed-off-by: Kevin Grigorenko <kevin.grigorenko@us.ibm.com>

---
+++ lib/create_all.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set -e
+oc apply -f lib/example_surveyinputservice.yaml && \
+  sleep 30 && \
+  kn service list surveyinputservice && \
+  oc apply -f lib/example_surveyadminservice.yaml && \
+  sleep 30 && \
+  kn service list surveyadminservice && \
+  oc apply -f lib/example_surveyadminkafkasource.yaml && \
+  sleep 30 && \
+  kn source kafka describe geocodetopicsource && \
+  oc apply -f lib/example_surveygeocoderservice.yaml && \
+  sleep 30 && \
+  kn service list surveygeocoderservice && \
+  oc apply -f lib/example_surveygeocoderkafkasource.yaml && \
+  sleep 30 && \
+  kn source kafka describe locationtopicsource
