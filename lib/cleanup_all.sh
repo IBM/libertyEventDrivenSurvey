a3bc9b50d3cf145862d9f18dbe9ad171ccad82ba
Kevin Grigorenko <kevin.grigorenko@us.ibm.com>
2023-10-25 11:34:34 -0500

    Update script
    Signed-off-by: Kevin Grigorenko <kevin.grigorenko@us.ibm.com>

--- lib/cleanup_all.sh
+++ lib/cleanup_all.sh
@@ -4,4 +4,6 @@ kn source kafka delete geocodetopicsource && \
   kn service delete surveyadminservice && \
   kn source kafka delete locationtopicsource && \
   kn service delete surveygeocoderservice && \
-  kn service delete surveyinputservice
+  kn service delete surveyinputservice && \
+  echo "Waiting for pods to terminate..." && \
+  sleep 30
